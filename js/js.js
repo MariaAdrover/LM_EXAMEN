bbe191d5945ad1fb1572dede0a01ed44b3ca1a83
MariaAdrover <32370320+MariaAdrover@users.noreply.github.com>
2018-02-01 02:12:37 +0100

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -23,6 +23,9 @@ var xmlDoc = null;
 var url = "https://rawgit.com/MariaAdrover/LM_EXAMEN/master/formulari.xml";
 var xhttp = new XMLHttpRequest();
 
+var preguntaXML;
+var preguntaHTML;
+
 
 xhttp.onreadystatechange = function() {
 
@@ -35,9 +38,86 @@ xhttp.open("GET", url, false);
 xhttp.send();
 
 window.onload = function(){
+	preguntaXML = xmlDoc.getElementsByTagName("question");
+	preguntaHTML = document.getElementsByClassName('pregunta');
+	
 	gestionarXml();
 }
 
-function gestionarXml() {
-	document.getElementById("preg1").innerHTML = xmlDoc.getElementsByTagName("title")[0].innerHTML;
- }
+function gestionarXml() {	
+	var tituloHTML = document.getElementsByClassName("title");
+	var tituloXML = xmlDoc.getElementsByTagName("title");	
+	var i;
+	var j;
+	var numOpciones;
+		
+	for (i = 0; i < preguntaXML.length; i++) {
+		tituloHTML[i].innerHTML = tituloXML[i].innerHTML;
+		numOpciones = preguntaXML[i].getElementsByTagName('option').length;
+		
+		if (xmlDoc.getElementsByTagName('type')[i].innerHTML == 'select' || xmlDoc.getElementsByTagName('type')[i].innerHTML == 'multiple'){						
+			var optionXML = [];
+			
+			optionXML = leerOptions (numOpciones, i);
+			llenarSelect (optionXML, i);
+		}
+		
+		if (xmlDoc.getElementsByTagName('type')[i].innerHTML == 'checkbox') {			
+			var optionXML = [];
+			
+			optionXML = leerOptions (numOpciones, i);
+			llenarCheckbox (optionXML, i);
+		}
+		
+		/* O es mejor...
+		if (i==2 || i==3){
+			
+		}...*/		
+	}	
+}
+
+function leerOptions (numOpciones, i) {
+	var optionXML = [];
+	
+	for (j=0; j<numOpciones; j++) { 
+		optionXML[j] =  preguntaXML[i].getElementsByTagName("option")[j].innerHTML;
+	}
+	
+	return optionXML;	
+}
+
+function llenarCheckbox (optionXML, pos) {
+	
+	// Es necesario poner los div?
+
+	for (i = 0; i < optionXML.length; i++) {
+		var input = document.createElement("input");
+		var label = document.createElement("label");
+
+		label.innerHTML = optionXML[i];
+		label.setAttribute("for", "p" + pos + "_" + i);
+		input.type = "checkbox";
+		input.name = "p" + pos;
+		input.id = "p" + pos + "_" + i; 
+		preguntaHTML[pos].appendChild(input);
+		preguntaHTML[pos].appendChild(label);
+		preguntaHTML[pos].appendChild(document.createElement("br"));
+	}	
+}
+
+function llenarRadio () {
+	
+}
+
+function llenarSelect (optionXML, pos) {
+	var select = preguntaHTML[pos].getElementsByTagName('select')[0];
+	
+	for (i = 0; i < optionXML.length; i++) {
+		var option = document.createElement("option");
+		option.text = optionXML[i];
+		option.value = i+1;		
+		select.options.add(option);		
+	}
+}
+ 
+
