7d3a39f89ee731897c5431d4433fd838a7d9b963
MariaAdrover <32370320+MariaAdrover@users.noreply.github.com>
2018-02-15 13:52:47 +0100

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,4 +1,22 @@
-/*var xhttp = new XMLHttpRequest();
+var xhttp = new XMLHttpRequest();
+
+var url = "https://rawgit.com/MariaAdrover/LM_EXAMEN/master/formulari.xml";
+var urlJavi = 'https://rawgit.com/shamshir/Prueba/master/questions.xml';
+
+var xmlDoc = null;
+var preguntaXML;
+var preguntaHTML;
+
+var respuestas0 = [];
+var respuestas1 = [];
+var respuestas2 = [];
+var respuestas3 = [];
+var respuestas4 = [];
+var respuestas5 = [];
+var respuestas6 = [];
+var respuestas7 = [];
+var respuestas8 = [];
+var respuestas9 = [];
 
 // Peticion?
 xhttp.onreadystatechange = function() {
@@ -7,21 +25,132 @@ xhttp.onreadystatechange = function() {
  }
 };
 
-xhttp.open("GET", "https://rawgit.com/MariaAdrover/LM_EXAMEN/master/formulari.xml", true); //localhost no ha de funcionar
+xhttp.open("GET", url, true); //localhost no ha de funcionar
 xhttp.send();
 
-window.onload = function(){
-	gestionarXml();
-}
+
 
 function gestionarXml(dadesXml){
 var xmlDoc = dadesXml.responseXML;
-document.getElementsByClassName('pregunta')[0].innerHTML = xmlDoc.getElementsByTagName('title')[0].innerHTML;
-}*/
+
+	
+	preguntaXML = xmlDoc.getElementsByTagName("question");
+	preguntaHTML = document.getElementsByClassName('pregunta');
+	
+	var tituloHTML = document.getElementsByClassName("title");
+	var tituloXML = xmlDoc.getElementsByTagName("title");	
+	var i;
+	var j;
+	var numOpciones;
+	var numRespuestas;
+		
+	for (i = 0; i < preguntaXML.length; i++) {
+		tituloHTML[i].innerHTML = tituloXML[i].innerHTML;
+		numOpciones = preguntaXML[i].getElementsByTagName('option').length;
+		numRespuestas = preguntaXML[i].getElementsByTagName('answer').length;
+		
+		if (xmlDoc.getElementsByTagName('type')[i].innerHTML == 'select' || xmlDoc.getElementsByTagName('type')[i].innerHTML == 'multiple'){						
+			var optionXML = [];
+			
+			optionXML = leerOptions (numOpciones, i);
+			llenarSelect (optionXML, i);
+		}
+		
+		if (xmlDoc.getElementsByTagName('type')[i].innerHTML == 'checkbox') {			
+			var optionXML = [];
+			
+			optionXML = leerOptions (numOpciones, i);
+			llenarCheckbox (optionXML, i);
+		}
+		
+		if (xmlDoc.getElementsByTagName('type')[i].innerHTML == 'radio') {
+			var optionXML = [];
+			
+			optionXML = leerOptions (numOpciones, i);
+			llenarRadio (optionXML, i);
+		}
+		
+		for (j = 0; j < numRespuestas; j++) {
+			//eval('respuestas' + i + '[' + j + '] = preguntaXML[i].getElementsByTagName('answer')[j].innerHTML');
+			window['respuestas' + i][j] = preguntaXML[i].getElementsByTagName('answer')[j].innerHTML;
+			
+		}
+		
+	}
+
+}
+
+
+function leerOptions (numOpciones, i) {
+	var optionXML = [];
+	
+	for (j=0; j<numOpciones; j++) { 
+		optionXML[j] =  preguntaXML[i].getElementsByTagName("option")[j].innerHTML;
+	}
+	
+	return optionXML;	
+}
+
+function llenarCheckbox (optionXML, pos) {
+
+	for (i = 0; i < optionXML.length; i++) {
+		var input = document.createElement("input");
+		var label = document.createElement("label");
+
+		label.innerHTML = optionXML[i];
+		label.setAttribute("for", "p" + pos + "_" + i);
+		input.type = "checkbox";
+		input.value = i + 1;
+		input.name = "p" + pos + "_" + i;
+		input.id = "p" + pos + "_" + i;
+		
+		preguntaHTML[pos].appendChild(input);
+		preguntaHTML[pos].appendChild(label);
+		preguntaHTML[pos].appendChild(document.createElement("br"));
+	}	
+}
+
+function llenarRadio (optionXML, pos) {
+	
+	for (i = 0; i < optionXML.length; i++) {
+		var input = document.createElement ('input');
+		var label = document.createElement ('label');
+		
+		label.innerHTML = optionXML [i];
+		label.setAttribute('for', "p" + pos + "_" + i);
+		input.type = 'radio';
+		input.value = i + 1;
+		input.name = 'p' + pos;
+		input.id = "p" + pos + "_" + i;
+		
+		preguntaHTML[pos].appendChild(input);
+		preguntaHTML[pos].appendChild(label);		
+		preguntaHTML[pos].appendChild(document.createElement("br"));
+	}
+	
+}
+
+function llenarSelect (optionXML, pos) {
+	var select = preguntaHTML[pos].getElementsByTagName('select')[0];
+	
+	for (i = 0; i < optionXML.length; i++) {
+		var option = document.createElement("option");
+		option.text = optionXML[i];		
+		// option.innerHTML = optionXML[i]; Es lo mismo...
+		option.value = i + 1;		
+		select.options.add(option);		
+	}
+}
+
+
+
+
+/*
 
 var xmlDoc = null;
 var url = "https://rawgit.com/MariaAdrover/LM_EXAMEN/master/formulari.xml";
 var xhttp = new XMLHttpRequest();
+var urlJavi = 'https://rawgit.com/shamshir/Prueba/master/questions.xml';
 
 var preguntaXML;
 var preguntaHTML;
@@ -69,10 +198,12 @@ function gestionarXml() {
 			llenarCheckbox (optionXML, i);
 		}
 		
-		/* O es mejor...
-		if (i==2 || i==3){
+		if (xmlDoc.getElementsByTagName('type')[i].innerHTML == 'radio') {
+			var optionXML = [];
 			
-		}...*/		
+			optionXML = leerOptions (numOpciones, i);
+			llenarRadio (optionXML, i);
+		}
 	}	
 }
 
@@ -87,8 +218,6 @@ function leerOptions (numOpciones, i) {
 }
 
 function llenarCheckbox (optionXML, pos) {
-	
-	// Es necesario poner los div?
 
 	for (i = 0; i < optionXML.length; i++) {
 		var input = document.createElement("input");
@@ -97,15 +226,33 @@ function llenarCheckbox (optionXML, pos) {
 		label.innerHTML = optionXML[i];
 		label.setAttribute("for", "p" + pos + "_" + i);
 		input.type = "checkbox";
-		input.name = "p" + pos;
-		input.id = "p" + pos + "_" + i; 
+		input.value = i + 1;
+		input.name = "p" + pos + "_" + i;
+		input.id = "p" + pos + "_" + i;
+		
 		preguntaHTML[pos].appendChild(input);
 		preguntaHTML[pos].appendChild(label);
 		preguntaHTML[pos].appendChild(document.createElement("br"));
 	}	
 }
 
-function llenarRadio () {
+function llenarRadio (optionXML, pos) {
+	
+	for (i = 0; i < optionXML.length; i++) {
+		var input = document.createElement ('input');
+		var label = document.createElement ('label');
+		
+		label.innerHTML = optionXML [i];
+		label.setAttribute('for', "p" + pos + "_" + i);
+		input.type = 'radio';
+		input.value = i + 1;
+		input.name = 'p' + pos;
+		input.id = "p" + pos + "_" + i;
+		
+		preguntaHTML[pos].appendChild(input);
+		preguntaHTML[pos].appendChild(label);		
+		preguntaHTML[pos].appendChild(document.createElement("br"));
+	}
 	
 }
 
@@ -114,10 +261,11 @@ function llenarSelect (optionXML, pos) {
 	
 	for (i = 0; i < optionXML.length; i++) {
 		var option = document.createElement("option");
-		option.text = optionXML[i];
-		option.value = i+1;		
+		option.text = optionXML[i];		
+		// option.innerHTML = optionXML[i]; Es lo mismo...
+		option.value = i + 1;		
 		select.options.add(option);		
 	}
 }
- 
+*/
 
